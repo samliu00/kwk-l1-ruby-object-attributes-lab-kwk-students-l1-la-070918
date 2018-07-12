18d768dee54d8397a68461ecfa54646897607ec8
samliu00 <samanthaliu19@gmail.com>
2018-07-12 22:35:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,6 +10,7 @@ end
 def name
   @name = "Fido"
 end
+
 def name=(new_name)
   @name = new_name 
 end
@@ -17,6 +18,7 @@ end
 def breed
   @breed = "Beagle"
 end 
+
 def breed = (new_breed)
   @breed = new_breed 
 end 
