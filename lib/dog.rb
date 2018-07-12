52d4bd7916bbd1ebbffb58277af55dedbfb4b832
samliu00 <samanthaliu19@gmail.com>
2018-07-12 22:40:03 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # dog.rb
+
+class Dog  
+
+def initalize(name, breed)
+  @name = name
+  @breed = breed
+end
+
+def name
+  @name = "Fido"
+end
+
+def name=(new_name)
+  @name = new_name 
+end
+  
+def breed
+  @breed = "Beagle"
+end 
+
+def breed=(new_breed)
+  @breed = new_breed 
+end 
+  
+end 
